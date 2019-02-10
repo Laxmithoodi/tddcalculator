13780f042c2ad02b88085fe68d0bc656911bce82
sylram <sylram7@gmail.com>
2019-02-10 17:28:49 -0500

    "Console created, Userinterfaced 3 calcs"

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -1,5 +1,16 @@
+import java.io.Console;
+import java.io.*;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
 
 public class MainApplication {
 
+    public static void main(String[] args) throws IOException {
+        UserInterface ui = new UserInterface();
+        ui.startInterface();
     }
+
+}
 
--- src/test/java/UserOperationsTest.java
+++ src/test/java/UserOperationsTest.java
@@ -1,7 +1,7 @@
 public class UserOperationsTest {
 
     public void testAdd1(){
-        x = 10;
+        int x = 10;
         int y= 5;
         UserOperations user = new UserOperations();
 
